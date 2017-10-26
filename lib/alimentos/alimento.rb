4487dcc15ddf41bcf23f0e96d125fd2cda37c4f4
Imar Abreu Diaz <alu0100888447@ull.edu.es>
2017-10-26 18:49:58 +0100

    Arrlego del fallo con la creacion del initialize

--- lib/alimentos/alimento.rb
+++ lib/alimentos/alimento.rb
@@ -1,3 +1,8 @@
 class Alimento
-
+  def initialize (nombre, proteinas, glucidos, lipidos)
+    @nombre = nombre
+    @proteinas = proteinas
+    @glucidos = glucidos
+    @lipidos = lipidos
+  end
 end
