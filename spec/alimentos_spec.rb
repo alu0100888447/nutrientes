556720f2c619d269592c137647bce9da0b47875f
Imar Abreu Diaz <alu0100888447@ull.edu.es>
2017-10-26 18:48:20 +0100

    Fallo en la inicializacion del objeto @huevoFrito

--- spec/alimentos_spec.rb
+++ spec/alimentos_spec.rb
@@ -1,11 +1,10 @@
 require "spec_helper"
-
+require "./lib/alimentos/alimento"
 RSpec.describe Alimentos do
   it "has a version number" do
     expect(Alimentos::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  before :each do
+    @huevoFrito = Alimento.new("Huevo Frito", 14.1, 0.0, 19.5)
   end
 end
