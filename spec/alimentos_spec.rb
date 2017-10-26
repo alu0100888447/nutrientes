6d8f5d051c9183a8657fe59ed163fe67ccb80b89
Imar Abreu Diaz <alu0100888447@ull.edu.es>
2017-10-26 20:29:25 +0100

    Finalizacion de las pruebas

--- spec/alimentos_spec.rb
+++ spec/alimentos_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "./lib/alimentos/alimento"
 RSpec.describe Alimentos do
 
-  before :each do
+  before :all do
     @huevoFrito = Alimento.new("Huevo Frito", 14.1, 0.0, 19.5)
     @lecheVaca = Alimento.new("Leche Vaca", 3.3, 4.8, 3.2)
     @yogurt = Alimento.new("Yogurt", 3.8, 4.9, 3.8)
@@ -49,8 +49,62 @@ RSpec.describe Alimentos do
   end
 
   describe "# Calculo del valor nutricional" do
-    it "Valor nutricional correcto" do
+    it "Valor nutricional Huevo Frito" do
       expect(@huevoFrito.valorNutricional).to eq(231.9)
+    end
+    it "Valor nutricional Leche de VAca" do
+      expect(@lecheVaca.valorNutricional).to eq(61.2)
+    end
+    it "Valor nutricional Yogurt" do
+      expect(@yogurt.valorNutricional).to eq(69.0)
+    end
+    it "Valor nutricional Cerdo" do
+      expect(@cerdo.valorNutricional).to eq(142.7)
+    end
+    it "Valor nutricional Ternera" do
+      expect(@ternera.valorNutricional).to eq(112.30000000000001)
+    end
+    it "Valor nutricional Pollo" do
+      expect(@pollo.valorNutricional).to eq(132.8)
+    end
+    it "Valor nutricional Bacalao" do
+      expect(@bacalao.valorNutricional).to eq(74.39999999999999)
+    end
+    it "Valor nutricional Atun" do
+      expect(@atun.valorNutricional).to eq(225.5)
+    end
+    it "Valor nutricional Salmon" do
+      expect(@salmon.valorNutricional).to eq(202.0)
+    end
+    it "Valor nutricional Aceite de Oliva" do
+      expect(@aceiteOliva.valorNutricional).to eq(897.1999999999999)
+    end
+    it "Valor nutricional Chocolate" do
+      expect(@chocolate.valorNutricional).to eq(479.2)
+    end
+    it "Valor nutricional Azucar" do
+      expect(@azucar.valorNutricional).to eq(399.2)
+    end
+    it "Valor nutricional Arroz" do
+      expect(@arroz.valorNutricional).to eq(343.4)
+    end
+    it "Valor nutricional Lentejas" do
+      expect(@lentejas.valorNutricional).to eq(314.6)
+    end
+    it "Valor nutricional Papas" do
+      expect(@papas.valorNutricional).to eq(70.5)
+    end
+    it "Valor nutricional Tomate" do
+      expect(@tomate.valorNutricional).to eq(19.8)
+    end
+    it "Valor nutricional Cebolla" do
+      expect(@cebolla.valorNutricional).to eq(31.099999999999998)
+    end
+    it "Valor nutricional Manzana" do
+      expect(@manzana.valorNutricional).to eq(54.400000000000006)
+    end
+    it "Valor nutricional Platanos" do
+      expect(@platanos.valorNutricional).to eq(92.19999999999999)
     end
   end
 
