d6487649932d68b2fa341f2cb9418eaa6577a683
Imar Abreu Diaz <alu0100888447@ull.edu.es>
2017-11-09 19:16:06 +0000

    Creacion funcion psuh

--- lib/alimentos/lista.rb
+++ lib/alimentos/lista.rb
@@ -9,4 +9,19 @@ class Lista
     @size = 0
   end
 
+  def push (valor)
+    if (@size == 0)
+      nodo = Node.new(valor, nil, nil)
+      @tail = nodo
+    elsif @size == 1
+      nodo = Node.new(valor, nil, @tail)
+      @tail.next = nodo
+    else
+      nodo = Node.new(valor, nil, @head)
+      @head.next = nodo
+    end
+    @head = nodo
+    @size += 1
+  end
+
 end
